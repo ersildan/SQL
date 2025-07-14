4033db6c80eed7ecfd254ec0ad8c95a9bffe4e47
11fre <11free11@mail.ru>
2025-07-14 19:59:27 +0300

    +

--- small_course_SQL/lesson_1.py
+++ small_course_SQL/lesson_1.py
@@ -1,4 +1,11 @@
 import sqlite3
 
+"""Подключение к существующей базе данных"""
 
+db = sqlite3.connect("D:\programms\sql_subd\data\small_course.db") # Подключение к базе данных
+print("Подключились к БД")
+cur = db.cursor() # Переменная для управления базы данных
 
+cur.execute("SELECT * FROM students;") # запрос для получения содержимого таблицы Students
+result = cur.fetchall() # Результат запроса
+print(result)
