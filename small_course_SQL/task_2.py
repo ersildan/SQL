3e0f39b912a77956f2163d649cc22b65f50dc8d9
11fre <11free11@mail.ru>
2025-07-21 14:47:38 +0300

    update func update_balance_rub
    test print args

--- small_course_SQL/task_2.py
+++ small_course_SQL/task_2.py
@@ -38,47 +38,65 @@ exc_txt = """Курс валют:
 1 USD = 0,87 EUR
 1 EUR = 1,15 USD"""
 
-def update_balance_rub():
-    ...
-def update_balance_usd():
-    ...
-def update_balance_eur():
+def update_balance_rub(amount, balance, choice_2, data):
+    """Функция для обновления данных в Balance_RUB"""
+
+    dict_exc = {
+                '2': 70,
+                '3': 80
+    }
+
+    print(amount, balance, choice_2, data)
+    print()
+    print(dict_exc)
+
+
+def update_balance_usd(amount, balance, choice_2, data):
+    """Функция для обновления данных в Balance_USD"""
+
     ...
 
 
-def check_amount(amount, choice):
+def update_balance_eur(amount, balance, choice_2, data):
+    """Функция для обновления данных в Balance_EUR"""
+
+    ...
+
+
+
+def check_amount(amount, choice_1):
     """Проверка amount на правильность ввода и на остаток баланса"""
 
-    if amount.isdigit() is not True:
+    try:
+        amount = float(amount.replace(',', '.')) # Проверка числа на float()
+    except:
         raise Exception ('\033[31mОшибка\033[0m: Нужно вводить только цифры без букв')
 
     cur.execute("""SELECT * FROM users_balance;""") # Запрос в таблицу users_balance
 
     data = cur.fetchone() # Данные из первой строки в таблице users_balance
-    exchanger_balance = data[int(choice)] # Баланс выбранной валюты
-    exc = cur.description[int(choice)][0] # Наименование валюты
+    balance = data[int(choice_1)] # Баланс выбранной валюты
+    name_exc = cur.description[int(choice_1)][0] # Наименование валюты
 
-    if int(amount) > exchanger_balance:
-        raise Exception (f'\033[31mНедостаточно средств\033[0m: {exc} = {exchanger_balance}\n'
+    if amount > balance:
+        raise Exception (f'\033[31mНедостаточно средств\033[0m: {name_exc} = {balance}\n'
                          'Повторите попытку')
-    dict_exc = {
-                '1': 'RUB',
-                '2': 'USD',
-                '3': 'EUR'
-               }
-
-    print(*[f"Введите [ {key} ] для обмена на {dict_exc[key]}"
-           for key, value in dict_exc.items() if key != choice], sep='\n') # Выводим список доступных валют для обмена
-
+    d = {
+         '1': 'RUB',
+         '2': 'USD',
+         '3': 'EUR'
+    }
+    print(*[f"Введите [ {key} ] для обмена на {d[key]}"
+           for key, value in d.items() if key != choice_1], sep='\n') # Выводим список доступных валют для обмена
 
     choice_2 = input() # Выбор пользователя на что он будет менять свою валюту
     match choice_2:
         case '1':
-            update_balance_rub()
+            update_balance_rub(amount, balance, choice_2, data)
         case '2':
-            update_balance_usd()
+            update_balance_usd(amount, balance, choice_2, data)
         case '3':
-            update_balance_eur()
+            update_balance_eur(amount, balance, choice_2, data)
         case _:
             raise Exception ("Произошла ошибка. Соблюдайте правильность ввода.")
 
@@ -94,22 +112,23 @@ def func():
                    "Чтобы выйти введите любой символ")
             print(txt)
 
-            choice = input() # Выбор валюты от пользователя согласно тексту в txt
+            choice_1 = input() # Выбор валюты от пользователя согласно тексту в txt
 
-            if choice == '1':
-                print('Сколько меняем RUB?')
-            elif choice == '2':
-                print('Сколько меняем USD?')
-            elif choice == '3':
-                print('Сколько меняем EUR?')
-            elif choice == '4':
-                print(exc_txt)
-                raise Exception
-            else:
-                exit('Совершен выход из обменника')
+            match choice_1:
+                case '1':
+                    print('Сколько меняем RUB?')
+                case '2':
+                    print('Сколько меняем USD?')
+                case '3':
+                    print('Сколько меняем EUR?')
+                case '4':
+                    print(exc_txt)
+                    raise Exception
+                case _:
+                    exit('Совершен выход из обменника')
 
             amount = input() # Желаемая сумма для обмена на другую валюту
-            check_amount(amount, choice) # Проверка введенной суммы (amount)
+            check_amount(amount, choice_1) # Проверка введенной суммы (amount)
 
             break
         except Exception as e:
