20f7ba263002f14786e11d409c432ad68750bc23
11fre <11free11@mail.ru>
2025-07-22 13:48:27 +0300

    lesson 4

--- sql_atm/atm.py
+++ sql_atm/atm.py
@@ -6,13 +6,14 @@ class ATM():
     def atm_logic(self):
 
         SQL_atm.create_table()
-        SQL_atm.insert_users((1234, 1111, 10000))
+        # SQL_atm.insert_users((1234, 1111, 10000))
         number_card = input('Введите, пожалуйста, номер карты: ')
 
         while True:
             if SQL_atm.input_card(number_card):
                 if SQL_atm.input_code(number_card):
-                    print("2222")
+                    SQL_atm.info_balance(number_card)
+                    break
                 else:
                     break
             else:
