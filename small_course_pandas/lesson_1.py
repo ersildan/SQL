d37e8375f3f1ae4349bfb50cc48d5b0dd40fde86
11fre <11free11@mail.ru>
2025-08-01 23:06:06 +0300

    delete library

--- small_course_pandas/lesson_1.py
+++ small_course_pandas/lesson_1.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 
 '''Для начала познакомимся с базовыми структурами данных. В Pandas их всего двe: Series и DataFrame.
