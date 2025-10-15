af59b5c91a84e09ec5c8973a05faca67ef9fa992
Andrey Svistunov <11free11@mail.ru>
2025-10-15 20:20:41 +0300

    theme group by

--- SQL_karpov_course/4_group_by/10.sql
+++ SQL_karpov_course/4_group_by/10.sql
@@ -1,1 +1,16 @@
--- test
+SELECT array_length(product_ids, 1) as order_size,
+       count(order_id) as orders_count
+FROM   orders
+WHERE  creation_time >= '2022-08-29'
+   and creation_time < '2022-09-05'
+GROUP BY order_size
+ORDER BY order_size;
+
+
+-- SELECT array_length(product_ids, 1) as order_size,
+--        count(order_id) as orders_count
+-- FROM   orders
+-- WHERE  creation_time between '2022-08-29'
+--    and '2022-09-05'
+-- GROUP BY order_size
+-- ORDER BY order_size;
