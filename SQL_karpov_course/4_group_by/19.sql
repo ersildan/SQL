8598715f218a4004bb902e7fdd94946e017a0a86
Andrey Svistunov <11free11@mail.ru>
2025-10-24 11:46:46 +0300

    +

--- SQL_karpov_course/4_group_by/19.sql
+++ SQL_karpov_course/4_group_by/19.sql
@@ -1,9 +1,17 @@
-SELECT 
-    DATE_PART('isodow', time) :: integer AS weekday_number,
+SELECT
+    DATE_PART('isodow', time)::integer AS weekday_number,
     TO_CHAR(time, 'Dy') AS weekday,
-    COUNT(DISTINCT order_id) AS created_orders,
-    AS canceled_orders,
-    AS actual_orders,
-    AS success_rate
+    COUNT(DISTINCT order_id) FILTER (WHERE action = 'create_order') AS created_orders,
+    COUNT(DISTINCT order_id) FILTER (WHERE action = 'cancel_order') AS canceled_orders,
+    COUNT(DISTINCT order_id) FILTER (WHERE action = 'create_order') - 
+    COUNT(DISTINCT order_id) FILTER (WHERE action = 'cancel_order') AS actual_orders,
+    ROUND(
+        (COUNT(DISTINCT order_id) FILTER (WHERE action = 'create_order') - 
+         COUNT(DISTINCT order_id) FILTER (WHERE action = 'cancel_order'))::decimal / 
+        COUNT(DISTINCT order_id) FILTER (WHERE action = 'create_order'),
+        3
+    ) AS success_rate
 FROM user_actions
-GROUP BY weekday
+WHERE time BETWEEN '2022-08-24' AND '2022-09-06 23:59:59'
+GROUP BY weekday_number, weekday
+ORDER BY weekday_number;
